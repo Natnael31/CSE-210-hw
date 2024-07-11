9086ae2b1b33e4f237a7dd4f1558d6573ee3b088
NatnaelGashaw <natystd21@gmail.com>
2024-07-11 17:01:53 +0300

    learning 05 activity completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Square s = new Square("Green", 5);
+        Recatangle r = new Recatangle("Yellow", 5, 4);
+        Circle c = new Circle("Red", 5);
+
+        Console.WriteLine(s.GetColor());
+        Console.WriteLine(s.GetArea());
+        Console.WriteLine();
+
+        Console.WriteLine(r.GetColor());
+        Console.WriteLine(r.GetArea());
+        Console.WriteLine();
+
+        Console.WriteLine(c.GetColor());
+        Console.WriteLine(c.GetArea());
+        Console.WriteLine();
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Green", 5));
+        shapes.Add(new Recatangle("Yellow", 6, 8));
+        shapes.Add(new Circle("Red", 10));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color : {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+
+
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Recatangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    public Recatangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+
+}
